3156801c6a9f6bbe279c9d01dabb2967ce84ff89
Haitham Mahmoud <m.haitham2001@gmail.com>
2022-10-22 21:09:52 +0200

    Added checking for different signal lengths

--- DSPComponents/Algorithms/Adder.cs
+++ DSPComponents/Algorithms/Adder.cs
@@ -15,6 +15,21 @@ namespace DSPAlgorithms.Algorithms
         public override void Run()
         {
             List<float> signal_samples = new List<float>();
+            if(InputSignals[0].Samples.Count != InputSignals[1].Samples.Count)
+            {
+                int TheDifference = Math.Abs(InputSignals[0].Samples.Count - InputSignals[1].Samples.Count);
+                int MaxNumber = Math.Max(InputSignals[0].Samples.Count , InputSignals[1].Samples.Count);
+                if (InputSignals[0].Samples.Count > InputSignals[1].Samples.Count)
+                {
+                    for (int i = TheDifference; i < MaxNumber; i++)
+                        InputSignals[1].Samples.Add(0);
+                }
+                else if(InputSignals[0].Samples.Count < InputSignals[1].Samples.Count)
+                {
+                    for (int i = TheDifference; i < MaxNumber; i++)
+                        InputSignals[0].Samples.Add(0);
+                }
+            }
             for (int i = 0; i < InputSignals[1].Samples.Count; i++)
             {
                 signal_samples.Add(InputSignals[0].Samples[i] + InputSignals[1].Samples[i]);
--- DSPComponents/Algorithms/Subtractor.cs
+++ DSPComponents/Algorithms/Subtractor.cs
@@ -20,6 +20,21 @@ namespace DSPAlgorithms.Algorithms
         public override void Run()
         {
             OutputSignal = new Signal(new List<float>(), false);
+            if (InputSignal1.Samples.Count != InputSignal2.Samples.Count)
+            {
+                int TheDifference = Math.Abs(InputSignal1.Samples.Count - InputSignal2.Samples.Count);
+                int MaxNumber = Math.Max(InputSignal1.Samples.Count, InputSignal2.Samples.Count);
+                if (InputSignal1.Samples.Count > InputSignal2.Samples.Count)
+                {
+                    for (int i = TheDifference; i < MaxNumber; i++)
+                        InputSignal2.Samples.Add(0);
+                }
+                else if (InputSignal1.Samples.Count < InputSignal2.Samples.Count)
+                {
+                    for (int i = TheDifference; i < MaxNumber; i++)
+                        InputSignal1.Samples.Add(0);
+                }
+            }
             for (int i = 0; i < InputSignal1.SamplesIndices.Count; i++)
             {
                 OutputSignal.Samples.Add(InputSignal1.Samples[i] - InputSignal2.Samples[i]);
