373140b26fb7336b8c9635cd1c62bc2321bf1be6
Hani Mohamed Sayed Ahmed <HaniMohamedFcis2023@gmail.com>
2022-10-29 00:10:48 +0200

    update Quantiaztion

--- DSPComponents/Algorithms/QuantizationAndEncoding.cs
+++ DSPComponents/Algorithms/QuantizationAndEncoding.cs
@@ -19,9 +19,48 @@ namespace DSPAlgorithms.Algorithms
         public List<string> OutputEncodedSignal { get; set; }
         public List<float> OutputSamplesError { get; set; }
 
+        public struct Range
+        {
+            public float Start, End, Midpoint;
+        }
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputEncodedSignal = new List<string>();
+            OutputIntervalIndices = new List<int>();
+            OutputQuantizedSignal = new Signal(new List<float>(), false);
+            OutputSamplesError = new List<float>();
+
+            if (InputNumBits > 0)
+                InputLevel = (int)Math.Pow((double)2, (double)InputNumBits);
+            else if (InputLevel > 0)
+                InputNumBits = (int)Math.Log(InputLevel, 2);
+
+            float Max_Amp = InputSignal.Samples.Max();
+            float Min_Amp = InputSignal.Samples.Min();
+            float res = (Max_Amp - Min_Amp) / InputLevel;
+
+            List<Range> Level = new List<Range>();
+            for (int i = 1; i < InputLevel + 1; i++)
+            {
+                Level[i].Start.Equals(Min_Amp);
+                Min_Amp += res;
+                Level[i].End.Equals(Min_Amp);
+                Level[i].Midpoint.Equals((Level[i].Start + Level[i].End) / 2);
+            }
+
+            for (int i = 0; i < InputSignal.Samples.Count(); i++)
+            {
+                for (int j = 1; j < InputLevel + 1; j++)
+                {
+                    if (InputSignal.Samples[i] >= Level[j].Start && InputSignal.Samples[i] <= Level[j].End)
+                    {
+                        OutputIntervalIndices.Add(j);
+                        OutputQuantizedSignal.Samples.Add(Level[j].Midpoint);
+                        OutputSamplesError.Add(Level[j].Midpoint - InputSignal.Samples[i]);
+                    }
+                }
+
+            }
         }
     }
 }
