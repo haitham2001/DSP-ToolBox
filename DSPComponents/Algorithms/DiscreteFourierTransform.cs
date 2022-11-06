a158e30754ef32f76f0bf78fc4248b1a024a06a5
Haitham Mahmoud <m.haitham2001@gmail.com>
2022-11-06 02:07:15 +0200

    Task 3 done

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -7,6 +7,16 @@ using DSPAlgorithms.DataStructures;
 
 namespace DSPAlgorithms.Algorithms
 {
+    public struct Complex1
+    {
+        public double RealNum;
+        public double ImaginaryNum;
+        public Complex1(double RealNum, double ImaginaryNum)
+        {
+            this.RealNum = RealNum;
+            this.ImaginaryNum = ImaginaryNum;  
+        }
+    }
     public class DiscreteFourierTransform : Algorithm
     {
         public Signal InputTimeDomainSignal { get; set; }
@@ -15,7 +25,25 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputFreqDomainSignal = new Signal(false, new List<float>(), new List<float>(), new List<float>());
+            int SizeOfSignals = InputTimeDomainSignal.Samples.Count;
+            for (int k = 0; k < SizeOfSignals; k++)
+            {
+                Complex1 SingleFreq = new Complex1(0,0);
+                int n = 0;
+                foreach (float sample in InputTimeDomainSignal.Samples)
+                {
+                    double SingleSample = (2 * k * n * Math.PI) / SizeOfSignals;
+                    SingleFreq.RealNum += sample * Math.Cos(SingleSample);
+                    SingleFreq.ImaginaryNum += sample * Math.Sin(SingleSample) * -1;
+                    n++;
+                }
+                double Amplitude = Math.Sqrt((SingleFreq.RealNum * SingleFreq.RealNum) + (SingleFreq.ImaginaryNum * SingleFreq.ImaginaryNum));
+                double PhaseShift = Math.Atan2(SingleFreq.ImaginaryNum , SingleFreq.RealNum);
+                OutputFreqDomainSignal.Frequencies.Add(k);
+                OutputFreqDomainSignal.FrequenciesAmplitudes.Add((float)Amplitude);
+                OutputFreqDomainSignal.FrequenciesPhaseShifts.Add((float)PhaseShift);
+            }
         }
     }
 }
--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,25 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputTimeDomainSignal = new Signal(new List<float>(), false);
+            List<Complex> Samples = new List<Complex>();
+            int SizeOfSignals = InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+            for(int i = 0;i<SizeOfSignals;i++)
+            {
+                Samples.Add(Complex.FromPolarCoordinates(InputFreqDomainSignal.FrequenciesAmplitudes[i], InputFreqDomainSignal.FrequenciesPhaseShifts[i]));
+            }
+            for (int i = 0; i < SizeOfSignals; i++)
+            {
+                int n = 0;
+                Complex sum = new Complex(0, 0);
+                foreach (Complex Sample in Samples)
+                {
+                    double SingleSample = (2 * i * n * Math.PI) / SizeOfSignals;
+                    sum += Sample * (Math.Cos(SingleSample) + (Complex.ImaginaryOne*Math.Sin(SingleSample)));
+                    n++;
+                }
+                OutputTimeDomainSignal.Samples.Add((float)sum.Real/SizeOfSignals);
+            }
         }
     }
 }
