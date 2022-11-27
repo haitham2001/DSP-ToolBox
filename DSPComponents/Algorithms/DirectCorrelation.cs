be8e4a3ba3f2a7fdb65a7c747b1d1d02bd38300a
Hani Mohamed Sayed Ahmed <HaniMohamedFcis2023@gmail.com>
2022-11-27 21:23:00 +0200

    direct correlation

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -14,9 +14,55 @@ namespace DSPAlgorithms.Algorithms
         public List<float> OutputNonNormalizedCorrelation { get; set; }
         public List<float> OutputNormalizedCorrelation { get; set; }
 
+        public List<float> correlation(Signal FirstInput, Signal SecondInput)
+        {
+            int i = 0;
+            List<float> Output = new List<float>();
+            do
+            {
+                float head = 0;
+                List<float> multiplied = new List<float>();
+                for (int j = 0; j < FirstInput.Samples.Count; j++)
+                    multiplied.Add(FirstInput.Samples[j] * SecondInput.Samples[j]);
+
+                if (FirstInput.Periodic == true)
+                {
+                    head = SecondInput.Samples[0];
+                    SecondInput.Samples.Add(head);
+                    SecondInput.Samples.RemoveAt(0);
+                }
+                else
+                {
+                    SecondInput.Samples.Add(head);
+                    SecondInput.Samples.RemoveAt(0);
+                }
+                Output.Add(multiplied.Sum() / FirstInput.Samples.Count);
+                i++;
+            } while (i < FirstInput.Samples.Count);
+            return Output;
+        }
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputNormalizedCorrelation = new List<float>();
+            float squareRoot = 0;
+            if (InputSignal2 == null)
+            {
+                Signal newSignal = new Signal(new List<float>(InputSignal1.Samples), InputSignal1.Periodic);
+                OutputNonNormalizedCorrelation = correlation(InputSignal1, newSignal);
+                for (int i = 0; i < InputSignal1.Samples.Count; i++)
+                    squareRoot += InputSignal1.Samples[i] * InputSignal1.Samples[i];
+                squareRoot /= InputSignal1.Samples.Count;
+            }
+            else
+            {
+                OutputNonNormalizedCorrelation = correlation(InputSignal1, InputSignal2);
+                List<float> r1 = correlation(InputSignal1, InputSignal1);
+                List<float> r2 = correlation(InputSignal2, InputSignal2);
+                squareRoot = (float)Math.Sqrt(r1[0] * r2[0]);
+            }
+            for (int i = 0; i < InputSignal1.Samples.Count; i++)
+                OutputNormalizedCorrelation.Add((OutputNonNormalizedCorrelation[i] / squareRoot));
         }
+        
     }
 }
---
+++ DSPComponents/Algorithms/TimeDelay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class TimeDelay:Algorithm
+    {
+        public Signal InputSignal1 { get; set; }
+        public Signal InputSignal2 { get; set; }
+        public float InputSamplingPeriod { get; set; }
+        public float OutputTimeDelay { get; set; }
+
+        public override void Run()
+        {
+            DirectCorrelation dc = new DirectCorrelation();
+            dc.InputSignal1 = InputSignal1;
+            dc.Run();
+            float Max = dc.OutputNonNormalizedCorrelation.Max();
+
+            List<float> multiplied = new List<float>();
+            for (int j = 0; j < InputSignal1.Samples.Count; j++)
+                multiplied.Add(InputSignal1.Samples[j] * InputSignal2.Samples[j]);
+
+            int IndexOfMaxMult = dc.OutputNonNormalizedCorrelation.IndexOf(multiplied.Sum());
+            int IndexOfMax = dc.OutputNonNormalizedCorrelation.IndexOf(Max);
+            OutputTimeDelay = (IndexOfMax - IndexOfMaxMult) * InputSamplingPeriod;
+        }
+    }
+}
